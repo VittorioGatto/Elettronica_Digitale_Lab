a012ceadb259d22750597219acbb54d0d9655daa
emagroppo <50966465+emagroppo@users.noreply.github.com>
2020-04-02 21:37:34 +0200

    Update circuitB.vhd

--- 02_Lab/part3/circuitB.vhd
+++ 02_Lab/part3/circuitB.vhd
@@ -12,9 +12,13 @@ architecture Behavior of circuitB is
 begin
 	-- remember led is grounded to cathode (negative logic)
 	-- if circuit is enabled (EN = 1), lit number "1"
-	HEX1(1) <= NOT EN;
-	HEX1(2) <= NOT EN;
-	HEX1(0) <= '1'; --off by default
-	HEX1(3 to 6) <= "1111"; -- off by default
+	HEX1(1) <= '0';
+	HEX1(2) <= '0'; --always on
+	HEX1(6) <= '1'; --always off
+	HEX1(3) <= EN;
+	HEX1(4) <= EN;
+	HEX1(5) <= EN;
+	HEX1(0) <= EN;
+
 end Behavior;
 	

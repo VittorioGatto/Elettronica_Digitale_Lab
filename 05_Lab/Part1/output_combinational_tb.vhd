636e698ec0132b429145c56944ce70d45b272d09
emagroppo <50966465+emagroppo@users.noreply.github.com>
2020-04-16 14:26:54 +0200

    Update output_combinational_tb.vhd

--- 05_Lab/Part1/output_combinational_tb.vhd
+++ 05_Lab/Part1/output_combinational_tb.vhd
@@ -6,7 +6,7 @@ end output_combinational_tb;
 
 architecture Behavior of output_combinational_tb is
 component output_combinational
-  port( idle, det0_1,det0_2,det0_3,det0_4,det1_1,det1_2,det1_3,det1_4: in std_logic;
+  port( det0_4,det1_4: in std_logic;
         z: out std_logic
       );
 end component;
@@ -19,15 +19,8 @@ DUT: output_combinational port map (idle_in,det0_1_in,det0_2_in,det0_3_in,det0_4
 
 process
   begin
-    idle_in <= '1', '0' after 5 ns;
-    det0_1_in <= '0', '1' after 5 ns, '0' after 10 ns;
-    det0_2_in <= '0', '1' after 10 ns, '0' after 15 ns;
-    det0_3_in <= '0', '1' after 15 ns, '0' after 20 ns;
     det0_4_in <= '0', '1' after 20 ns, '0' after 30 ns; --z = '1' for 20 ns < t < 30 ns
-    det1_1_in <= '0';
-    det1_2_in <= '0';
-    det1_3_in <= '0', '1' after 30 ns, '0' after 35 ns;
-    det1_4_in <= '0', '1' after 35 ns;
+    det1_4_in <= '0', '1' after 35 ns; --z = '1' for 35 ns < t < 40 ns
     wait for 40 ns;
 end process;
 end Behavior;

881a3f59da03731ee8a6df41caf07bc794325626
Lorenzo Riggi <lorenzoriggi1@gmail.com>
2020-04-25 21:46:19 +0200

    Update PseudoCode.c

--- 06_Lab/algorithm/PseudoCode.c
+++ 06_Lab/algorithm/PseudoCode.c
@@ -1,6 +1,9 @@
 //
+//  main.c
 //  PseudoCode
 //
+//  Created by Lorenzo Riggi on 25/04/2020.
+//  Copyright © 2020 Lorenzo Riggi. All rights reserved.
 //
 
 #include <stdio.h>
@@ -21,11 +24,24 @@ int previousXsum = 0; //previous sum of x's(sommatoria Ki)
 
 void writeMEM_A(int data_in[]);
 int processX(int x); //process datapath, returns y (see scheme)
+int checkErrorInteger(int x, int y); //returns correction
+int checkSaturation(int data); //returns data
 void writeMEM_B(int y);
-void generateRandoms(int lower, int upper);
+
+void generateRandoms(int lower, int upper); //remove before pasting
 
 int main(){
     int y;
+    int yProcessedSaturation;
+    
+    /*
+    int test [1024] = { 0 };
+    test[0] = 18;
+    test[1] = 2;
+    test[2] = 5;
+    test[3] = 17;
+     writeMEM_A(test);
+     */
     
     generateRandoms(-128, 127); //remove befor pasting
     writeMEM_A(data_in);
@@ -34,7 +50,8 @@ int main(){
         current_address = i;
         
         y = processX(MEM_A[i]);
-        writeMEM_B(y);
+        yProcessedSaturation = checkSaturation(y);
+        writeMEM_B(yProcessedSaturation);
         
         previousXsum = MEM_A[i] + previousX;
         previousX = MEM_A[i];
@@ -42,7 +59,7 @@ int main(){
     
     done = 1;
     
-    printf("-------OUTPUT DATA---------\n");
+    printf("\n\n=========  OUTPUT DATA  =========\n\n");
     for(int i = 0; i < 1024; i++)
         printf("%d \n", MEM_B[i]);
     
@@ -65,9 +82,53 @@ int processX(int x){
     wr = 0;
     rd = 1; // reading MEM_A
     
-    int y = 3.75*x + 2*(x + previousXsum) + 0.5*(x - previousX);
+    int Kp = (x * 4) - (x/4);
+    int Kd = 0.5*(x - previousX);
+    //int correction = checkErrorInteger(Kd, Kp);
+    
+    int y = Kp + 2*(x + previousXsum) + Kd;
+    //int y = Kp + 2*(x + previousXsum) + Kd + correction;
     
     return y;
+}
+
+int checkErrorInteger(int x, int y) {
+    if((x ^ y) >= 0 ) { //check if same sign (XOR)
+        if((y % 3) == 0 && y > 0) { //Multiple of 3 and positive
+            return 1;
+        } else if((y % 3) == 0 && y < 0) { //negative
+            return -1;
+        } else if((x % 2) != 0) { //Odd number
+            if((y % 4) != 0) { //Not multiple of 4
+                if(y > 0)
+                    return 1;
+                else
+                    return -1;
+            }
+        }
+    } else { //different signs
+        //x even and y multiple of 3
+        if(((x % 2) == 0) && ((y % 3) == 0)) {
+            if(x < 0 && y > 0) { //positive negative
+                return 1;
+            } else { //negative positive
+                return -1;
+            }
+        }
+    }
+    
+    return 0;
+}
+int checkSaturation(int data) {
+    if (data > 127){
+        printf("hey %d is saturated +, setting 999\n", data);
+        return 999; //let's assume this is the saturated value...
+    } else if (data < -128) {
+        printf("hey %d is saturated -, setting -999\n", data);
+        return -999;
+    }
+    
+    return data;
 }
 
 void writeMEM_B(int y){
@@ -86,5 +147,4 @@ void generateRandoms(int lower, int upper){
         printf("%d \n", data_in[i]);
     }
 }
-
 

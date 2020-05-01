c611aa9478c9f466dc5854ebd0b7876686864106
vittorio <vittoriogatto98@gmail.com>
2020-05-01 23:09:38 +0200

    new

--- 06_Lab/test/creazione_numeri.m
+++ 06_Lab/test/creazione_numeri.m
@@ -1,14 +1,28 @@
 clear all
 clc
 
-
+sum = 0;
 for i  = 1:1:1024
-    x(i,:) = bitget(randi(256) - 129,8:-1:1, 'int8');
+    dig_in(i) = randi(256) - 129;
+    sum = sum + dig_in(i);
+    if(i == 1)
+        dig_out(i) = 3.75*dig_in(i) + 2*sum + 0.5 * dig_in(i);
+    else
+    	dig_out(i) = 3.75*dig_in(i) + 2*sum + 0.5 * (dig_in(i)-dig_in(i-1));
+    end
+    bit(i,:) = bitget(dig_in(i),8:-1:1, 'int8');
 end
 
-fileID = fopen('test_data_in_bit','w');
+fileID1 = fopen('test_data_in','w');
+fileID2 = fopen('test_data_out','w');
+fileID3 = fopen('test_data_in_bit','w');
+
 for i  = 1:1:1024
-    fprintf(fileID,'%d',x(i,:));
-    fprintf(fileID,'\n');
+    fprintf(fileID1,'%d\n',dig_in(i));
+    fprintf(fileID2,'%d\n',dig_out(i));
+    fprintf(fileID3,'%d',bit(i,:));
+    fprintf(fileID3,'\n');
 end
-fclose(fileID);
+fclose(fileID1);
+fclose(fileID2);
+fclose(fileID3);

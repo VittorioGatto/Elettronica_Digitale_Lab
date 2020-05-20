9c7ad519350a586f30e32e5bb863bef86f9ddc28
vittorio <vittoriogatto98@gmail.com>
2020-05-20 12:20:32 +0200

    update_2047

--- 08_Lab/main1.1v2.c
+++ 08_Lab/main1.1v2.c
@@ -185,7 +185,7 @@ static void MX_TIM3_Init(void)
   /* USER CODE BEGIN TIM3_Init 1 */
 
   /* USER CODE END TIM3_Init 1 */
-  TIM_InitStruct.Prescaler = 2048;
+  TIM_InitStruct.Prescaler = 2047;
   TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
   TIM_InitStruct.Autoreload = 65535;
   TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
